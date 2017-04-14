6846ee73569e32b5ab898b93cbeb622e8175856d
Greg Silverman <greg.silverman@veritas.com>
2017-04-14 13:06:26 -0700

    test cases

--- ex.py
+++ ex.py
@@ -1,14 +1,27 @@
 import sys
 import subprocess
 
-bash = ['./bash','bash']
 
-params = r"'a' '';sleep 20''"
+def test(cmd):
+	print 'command:',' '.join(cmd)
+	sys.stdout.flush()
+	proc = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	proc.wait()
+	stdout,stderr = proc.communicate()
+	print stdout
+	print stderr
 
-cmd = 'echo'
-
-cmd_line = cmd +' ' + params
-
-proc = subprocess.Popen([bash[0],'-c',cmd_line])
-proc.wait()
-
+tests_cases = open(sys.argv[1],'r')
+i = 1
+for line in tests_cases:
+	print '----\ntest case '+str(i)
+	tokens = line.strip().split(' ')
+	cmd = ' '.join(tokens[1:])
+	print '--'
+	tokens = [None,tokens[0],cmd]
+	tokens[0] = '/bin/bash'
+	test(tokens)
+	print '--'
+	tokens[0] = './bash'
+	test(tokens)
+	i += 1
