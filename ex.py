f42e05ef1ed6a37342d9cbf645792de86c7ab97f
Greg Silverman <greg.silverman@veritas.com>
2017-04-17 13:31:21 -0700

    show time delta if sleep command was executed.

--- ex.py
+++ ex.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python
+
 import sys
 import subprocess
-
+import datetime
 
 def test(cmd):
 	print 'command:',' '.join(cmd)
 	sys.stdout.flush()
+	sleep_command_present = cmd[-1].find('sleep')!=-1
+        if sleep_command_present:
+		start_time = datetime.datetime.now()
 	proc = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	proc.wait()
 	stdout,stderr = proc.communicate()
 	print stdout
 	print stderr
+	if sleep_command_present:
+		stop_time = datetime.datetime.now()
+		print 'time delta: ', stop_time-start_time
 
 tests_cases = open(sys.argv[1],'r')
 i = 1
